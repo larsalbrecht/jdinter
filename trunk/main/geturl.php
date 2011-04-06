f46aa3aed53eec9e3a595262b441481510de22fe
lars.albrecht <lars.albrecht@79da5459-f7fb-5d22-1855-c391545c894e>
2011-04-06 15:36:03 +0000

    Start Groundfiles unsorted

---
+++ trunk/main/geturl.php
@@ -0,0 +1,43 @@
+<?php
+if($myURL = $_GET['url']){
+    print_r(curl_download(urldecode($_GET['url'])));
+}
+function curl_download($Url){
+ 
+    // is cURL installed yet?
+    if (!function_exists('curl_init')){
+        die('Sorry cURL is not installed!');
+    }
+ 
+    // OK cool - then let's create a new cURL resource handle
+    $ch = curl_init();
+ 
+    // Now set some options (most are optional)
+ 
+    // Set URL to download
+    curl_setopt($ch, CURLOPT_URL, $Url);
+ 
+    // Set a referer
+    curl_setopt($ch, CURLOPT_REFERER, "jdinter");
+ 
+    // User agent
+    curl_setopt($ch, CURLOPT_USERAGENT, "JDInterGetter");
+ 
+    // Include header in result? (0 = yes, 1 = no)
+    curl_setopt($ch, CURLOPT_HEADER, 0);
+ 
+    // Should cURL return or print out the data? (true = return, false = print)
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+ 
+    // Timeout in seconds
+    curl_setopt($ch, CURLOPT_TIMEOUT, 10);
+ 
+    // Download the given URL, and return output
+    $output = curl_exec($ch);
+ 
+    // Close the cURL resource, and free system resources
+    curl_close($ch);
+ 
+    return $output;
+}
+?>
